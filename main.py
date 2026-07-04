b503c1fd71726a73b4722bc939450a1d642d7625
Yasu Flores <yasu@fountain.com>
2026-07-04 10:11:40 -0500

    changes to train close to final neural net

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # hyperparameters
-batch_size = 32 # how many independent sequences will we process in parallel
-block_size = 8 # what is the maximum context length for predictions?
+batch_size = 64 # how many independent sequences will we process in parallel
+block_size = 256 # what is the maximum context length for predictions?
 max_iters = 5000 # number of training iterations for backpropagation
 eval_interval = 500
-learning_rate = 1e-3
+learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 32
+n_embd = 384
+n_head = 6
+n_layer = 6
+dropout = 0.2
 
 torch.manual_seed(1337)
 
@@ -65,7 +68,7 @@ class Head(nn.Module):
     self.query = nn.Linear(n_embd, head_size, bias=False)
     self.value = nn.Linear(n_embd, head_size, bias=False)
     self.register_buffer('trill', torch.tril(torch.ones(block_size, block_size)))
-
+    self.dropout = nn.Dropout(dropout)
 
   def forward(self, x):
     # input of size (batch, time-step, channels)
@@ -78,6 +81,7 @@ class Head(nn.Module):
     wei = q @ k.transpose(-2, -1) * k.shape[-1] ** -0.5 # (B, T, C) @ (B, C, T) -> (B, T, T)
     wei = wei.masked_fill(self.trill[:T, :T] == 0, float('-inf')) # (B, T, T)
     wei = F.softmax(wei, dim=-1) # (B, T, T)
+    wei = self.dropout(wei)
 
     # Perform the weighted aggretation of the values
     v = self.value(x)
@@ -91,6 +95,7 @@ class FeedForward(nn.Module):
     super().__init__()
     self.net = nn.Sequential(
       nn.Linear(n_embd, 4 * n_embd),
+      nn.Relu(),
       nn.Linear(4 * n_embd, n_embd),
       # nn.Dropout(dropout),
     )
@@ -104,9 +109,12 @@ class MultiHeadAttention(nn.Module):
   def __init__(self, num_heads, head_size):
     super().__init__()
     self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+    self.proj = nn.Linear(n_embd, n_embd)
 
   def forward(self, x):
-    return torch.cat([h(x) for h in self.heads], dim=-1)
+    out = torch.cat([h(x) for h in self.heads], dim=-1)
+    out = self.proj(out)
+    return out
 
 class Block(nn.Module):
   """ Transformer block: communication followed by computation """
@@ -117,10 +125,12 @@ class Block(nn.Module):
     head_size = n_embd // n_head
     self.sa = MultiHeadAttention(n_head, head_size)
     self.ffwd = FeedForward(n_embd)
+    self.ln1 = nn.LayerNorm(n_embd)
+    self.ln2 = nn.LayerNorm(n_embd)
 
   def forward(self, x):
-    x = self.sa(x)
-    x = self.ffwd(x)
+    x = x + self.sa(self.ln1(x))
+    x = x + self.ffwd(self.ln2(x))
     return x
 
 # Super simple bigram model
@@ -131,13 +141,13 @@ class BigramLanguageModel(nn.Module):
     # Each token directly reads off the logits for the next token from a lookup table
     self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
     self.position_embedding_table = nn.Embedding(block_size, n_embd)
-    self.blocks = nn.Sequential(
-      Block(n_embd, n_head = 4),
-      Block(n_embd, n_head = 4),
-      Block(n_embd, n_head = 4),
-    )
-    # self.sa_head = MultiHeadAttention(4, n_embd // 4)
-    # self.ffwed = FeedForward(n_embd)
+    self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)])
+    # self.blocks = nn.Sequential(
+    #   Block(n_embd, n_head = 4),
+    #   Block(n_embd, n_head = 4),
+    #   Block(n_embd, n_head = 4),
+    #   nn.LayerNorm(n_embd),
+    # )
     self.lm_head = nn.Linear(n_embd, vocab_size)
 
   def forward(self, idx, targets=None):
